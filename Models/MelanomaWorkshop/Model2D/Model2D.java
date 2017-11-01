e22db83d832de52996e368d7869e963b1a6450b5
Daniel Abler <dabler@BR12472.local>
2017-11-01 15:13:50 -0700

    multiple cells working; txt reading no longer

--- Models/MelanomaWorkshop/Model2D/Model2D.java
+++ Models/MelanomaWorkshop/Model2D/Model2D.java
@@ -1,44 +1,52 @@
+
+
 package Models.MelanomaWorkshop.Model2D;
 
 import Framework.Extensions.SphericalAgent2D;
 import Framework.GridsAndAgents.AgentGrid2D;
 import Framework.Gui.Vis2DOpenGL;
 import Framework.Utils;
-import sun.jvm.hotspot.oops.CellTypeState;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 import static Framework.Utils.*;
 
-// below: file reading
+// txt reading/writing
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
 
-
-
 class Dish extends AgentGrid2D<Cell> {
-    final static int BLACK=RGB(0,0,0),RED=RGB(1,0,0),GREEN=RGB(0,1,0),YELLOW=RGB(1,1,0),BLUE=RGB(0,0,1);
+    final static int BLACK=RGB(0,0,0),RED=RGB(1,0,0),GREEN=RGB(0,1,0),YELLOW=RGB(1,1,0),BLUE=RGB(0,0,1), AZUL=RGB(1,0,1);
     //GLOBAL CONSTANTS
     double DIVISION_PROB=0.01;
     double DEATH_PROB=0.0;
-    double CELL_RAD=0.3;
-    double MAX_RAD=Math.sqrt(2)*CELL_RAD;
+    double[] CELL_RAD={0.5,0.3,0.3,0.3};
+    double[] MAX_RAD=new double[CELL_RAD.length];
     double FRICTION=0.9;
     double STEADY_STATE_FORCE=0;
     double MAX_STEADY_STATE_LOOPS=10;
-    double DIV_RADIUS=CELL_RAD*(2.0/3.0);
+    double[] DIV_RADIUS=new double[CELL_RAD.length];
     double FORCE_EXPONENT=2;
     double FORCE_SCALER=0.7;
+    double immAttr; // alpha
+    double stromAttr; // beta
+    double[] divProb={0.01,0.001,0.01,0.01};
+    double[] deathProb={0.0,0.0,0.0,0.0};
+    double[] motility={0,3,1,1};
+
     //double MAX_VEL=1000000000;
 
+    int fusionCt=0;
     //INTERNAL VARIABLES
     Random rn=new Random();
     ArrayList<Cell> cellScratch=new ArrayList<>();
-    double[] divCoordScratch=new double[2];
+    ArrayList<Cell> cancerCells=new ArrayList<>();
+    ArrayList<Cell> stromaCells=new ArrayList<>();
+    ArrayList<Cell> immuneCells=new ArrayList<>();
 
     public List<String> initImageFile(String path_to_file){
         List<String> lines = new ArrayList<String>();
@@ -56,19 +64,39 @@ class Dish extends AgentGrid2D<Cell> {
     }
 
     public void writeToFile(List<String> array_list){
-        
+
     }
 
-    public Dish(int sideLen,int startingPop,double startingRadius){
+
+    double[] divCoordScratch=new double[2];
+
+    public Dish(int sideLen,int startingPop,int startingStroma,double startingRadius){
         super(sideLen,sideLen,Cell.class);
         double[] startCoords=new double[2];
-        for (int i = 0; i < startingPop; i++) {
-            Utils.RandomPointInCircle(startingRadius, startCoords, rn);
-            Cell c=NewAgentPT(startCoords[0]+xDim/2.0,startCoords[1]+yDim/2.0);
-            c.Init(RED);
+        for (int i = 0; i < CELL_RAD.length ; i++) {
+            MAX_RAD[i]=Math.sqrt(2)*CELL_RAD[i];
+            DIV_RADIUS[i]=CELL_RAD[i]*(2.0/3.0);
         }
-    }
 
+
+        double[] startCoordsStroma={10,10};
+        for (int i = 0; i < startingStroma; i++) {
+//            Utils.RandomPointInCircle(startingRadius, startCoords, rn);
+            Cell c=NewAgentPT(Math.round(Math.random()*xDim),Math.round(Math.random()*yDim));
+            c.Init(1);
+            stromaCells.add(c);
+        }
+
+        for (int i = 0; i < startingPop; i++) {
+//            Utils.RandomPointInCircle(startingRadius, startCoords, rn);
+            Cell c=NewAgentPT(xDim/2.0,yDim/2.0);
+//            Cell c=NewAgentPT(startCoords[0]+xDim/2.0,startCoords[1]+yDim/2.0);
+            c.Init(0);
+            cancerCells.add(c);
+        }
+
+
+    }
 
     int SteadyStateMovement(){
         int loopCt=0;
@@ -89,6 +117,16 @@ class Dish extends AgentGrid2D<Cell> {
         return loopCt;
     }
     void Step(){
+
+        //CHEMICALS
+        //update concentrations
+
+        //CELLS
+        //division
+        //death
+        //move
+
+
         SteadyStateMovement();
         //for (Cell c:this) {
         //    c.Observe();
@@ -105,26 +143,32 @@ class Dish extends AgentGrid2D<Cell> {
 }
 
 class Cell extends SphericalAgent2D<Cell,Dish> {
-    int color=G().RED;
-    String Type;
+    int color;
+    int type;
 
 
-    enum CELL_TYPE {
-        CANCER, IMMUNE, STROMA, MYELOID
-    }
 
-
-    void Init(int InitialColor){
-        radius=G().CELL_RAD;
+    void Init(int tType){
+        radius=G().CELL_RAD[tType];
+        type=tType;
         xVel=0;
         yVel=0;
-        color=InitialColor;
+        SetCellColor();
     }
 
-    void SetCellColor(int newColor){
-        color=newColor;
-    }
+    void SetCellColor(){
 
+        switch (type) {
+            case 0:  color = G().RED;
+                break;
+            case 1:  color = G().AZUL;
+                break;
+            case 2:  color = G().YELLOW;
+                break;
+            case 3:  color = G().YELLOW;
+                break;
+        }
+    }
 
     double OverlapToForce(double overlap){
         if(overlap<0){
@@ -136,7 +180,7 @@ class Cell extends SphericalAgent2D<Cell,Dish> {
 
     double Observe(){
 
-        return SumForces(radius+G().MAX_RAD,G().cellScratch,this::OverlapToForce);
+        return SumForces(radius+G().MAX_RAD[type],G().cellScratch,this::OverlapToForce);
         /*
         if(ret>G().MAX_VEL){
             xVel*=G().MAX_VEL/ret;
@@ -150,34 +194,34 @@ class Cell extends SphericalAgent2D<Cell,Dish> {
         ApplyFriction(G().FRICTION);
     }
     void Step(){
-        if(G().rn.nextDouble()<G().DEATH_PROB){
+        if(G().rn.nextDouble()<G().deathProb[type]){
             Dispose();
             return;
         }
-        if(G().rn.nextDouble()<G().DIVISION_PROB){
-            Cell child=Divide(G().DIV_RADIUS,G().divCoordScratch,G().rn);
-            child.Init(this.color);
-            Init(this.color);
+        if(G().rn.nextDouble()<G().divProb[type]){
+            Cell child=Divide(G().DIV_RADIUS[type],G().divCoordScratch,G().rn);
+            child.Init(this.type);
+            Init(this.type);
         }
     }
 }
 
-
 public class Model2D {
     static int SIDE_LEN=70;
     static int STARTING_POP=1;
-    static double STARTING_RADIUS=10;
-    static int TIMESTEPS=2000;
+    static int STARTING_STROMA=70;
+    static double STARTING_RADIUS=20;
+    static int TIMESTEPS=4000;
     static float[] circleCoords=Utils.GenCirclePoints(1,10);
     public static void main(String[] args) {
-        String path_to_file = "/Users/dabler/Documents/spatialstats/testdata.csv";
-
         //TickTimer trt=new TickRateTimer();
         Vis2DOpenGL vis=new Vis2DOpenGL("Cell Fusion Visualization", 1000,1000,SIDE_LEN,SIDE_LEN);
-        Dish d=new Dish(SIDE_LEN,STARTING_POP,STARTING_RADIUS);
-        List<String> list = d.initImageFile(path_to_file);
-
+        Dish d=new Dish(SIDE_LEN,STARTING_POP,STARTING_STROMA, STARTING_RADIUS);
         //d.SetCellsColor("red");
+
+        //String path_to_file = "/Users/dabler/Documents/spatialstats/testdata.csv";
+        //List<String> list = d.initImageFile(path_to_file);
+
         for (int i = 0; i < TIMESTEPS; i++) {
             vis.TickPause(0);
             d.Step();
